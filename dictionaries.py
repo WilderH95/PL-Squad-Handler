ffe4d5832a3052730955a62a7c413850bd770484
Harry Wilder <harrywilder95@hotmail.co.uk>
2025-06-18 15:56:13 +0100

    Add datahandler class and functionality to save df as csv.

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+data = {
+    'Opta ID' : [],
+    'Photo Name' : [],
+    'Squad Number' : [],
+    'First Name' : [],
+    'Surname' : []
+}
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datahandler import DataHandler
+
 
 def smart_capitalize(text):
     return ' '.join(word[0].upper() + word[1:] if word else '' for word in text.split())
@@ -11,25 +13,24 @@ soup = BeautifulSoup(request.text, features="html.parser")
 #Get Opta IDs from the HTML
 player_img_tag = soup.find_all(name='img', class_='statCardImg statCardPlayer')
 photo_ids =[player_img_tag[n]['data-player'] for n in range(len(player_img_tag))]
-print(photo_ids)
 
 #Create a list of Opta IDs by stripping the "p" out of the Photo IDs
 opta_ids = [n.strip("p") for n in photo_ids]
-print(opta_ids)
 
 #Get squad numbers from the HTML
 squad_number_div = soup.find_all(name="div", class_='stats-card__squad-number u-hide-mob-l')
 squad_numbers = [n.get_text() for n in squad_number_div]
-print(squad_numbers)
 
 #Get the first names from the HTML
 first_name_div = soup.find_all(name="div", class_='stats-card__player-first')
 first_names = [n.get_text() for n in first_name_div]
-print(first_names)
 
 #Get the last names from the HTML
 last_name_div = soup.find_all(name="div", class_='stats-card__player-last')
 last_names = [n.get_text() for n in last_name_div]
 #Ensure all last names start with a capital letter
 last_names = [smart_capitalize(n) for n in last_names]
-print(last_names)
+
+dh = DataHandler(opta_ids, photo_ids, squad_numbers, first_names, last_names)
+df = dh.create_df()
+df.to_csv("out.csv", index=False, encoding='utf-8-sig')
