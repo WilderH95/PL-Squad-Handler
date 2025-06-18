9e11181c9ab3d57144b0743573bc3f6d67d77a25
WilderH95 <harrywilder95@hotmail.co.uk>
2025-06-18 20:52:07 +0100

    Convert to Flask app. Build basic frontend and add basic error handling.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+from scraper import update_squad
+from dictionaries import *
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        team = request.form.get('team')
+        success, message = update_squad(team)
+        return render_template('index.html', squad_urls=squad_urls, message=message)
+
+    return render_template('index.html', squad_urls=squad_urls)
+
+if __name__ == '__main__':
+    app.run(debug=True)
