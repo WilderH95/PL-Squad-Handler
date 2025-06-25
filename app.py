6c912692c23820770b856e7aca80661f88df2d7d
Harry Wilder <harrywilder95@hotmail.co.uk>
2025-06-25 11:58:39 +0100

    Separate update_squad method into more manageable methods.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from datahandler import update_squad
+from datahandler import *
 from dictionaries import *
 
 app = Flask(__name__)
@@ -9,7 +9,9 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         team = request.form.get('team')
-        success, message = update_squad(team)
+        df = get_pl_squad(team)
+        sheet = open_sheet(team)
+        success, message = update_sheet(worksheet=sheet, dataframe=df, range='A6', team_name=team)
         return render_template('index.html', squad_urls=squad_urls, message=message)
 
     return render_template('index.html', squad_urls=squad_urls)
--- datahandler.py
+++ datahandler.py
@@ -19,79 +19,79 @@ def open_sheet(team_name):
                                "/edit?gid=977708243#gid=977708243")
     return sheet.worksheet(team_name)
 
-def smart_capitalize(text):
-    return ' '.join(word[0].upper() + word[1:] if word else '' for word in text.split())
-
-
-def update_squad(team_name):
+def update_sheet(worksheet, dataframe, range, team_name):
     try:
-        for key in data:
-            data[key].clear()
-
-        url = squad_urls[team_name]
-        request = requests.get(url)
-        soup = BeautifulSoup(request.text, features="html.parser")
-
-        # Parse the HTML to grab all the player cards from the squad page
-        all_players = soup.find_all('li', class_='stats-card')
-
-        # Initialise lists
-        photo_ids = []
-        squad_numbers = []
-        first_names= []
-        last_names = []
-
-        for player in all_players:
-            # Get Opta IDs from the HTML
-            photo_id = player.find(name='img', class_='statCardImg statCardPlayer')
-            if photo_id:
-                photo_ids.append(photo_id['data-player'])
-            else:
-                photo_ids.append(None)
-
-            # Get squad numbers from the HTML
-            squad_number = player.find(name="div", class_='stats-card__squad-number u-hide-mob-l')
-            if squad_number:
-                squad_numbers.append(squad_number.get_text())
-            else:
-                squad_numbers.append(None)
-
-            # Get the first names from the HTML
-            first_name = player.find(name="div", class_='stats-card__player-first')
-            if first_name:
-                first_names.append(first_name.get_text().strip())
-            else:
-                first_names.append(None)
-
-            # Get the last names from the HTML
-            last_name = player.find(name="div", class_='stats-card__player-last')
-            if last_name:
-                last_names.append(last_name.get_text().strip())
-            else:
-                last_names.append(None)
-
-        # Create a list of Opta IDs by stripping the "p" out of the Photo IDs
-        opta_ids = [n.strip("p") for n in photo_ids]
-
-        # Ensure all last names start with a capital letter
-        last_names = [smart_capitalize(n) for n in last_names]
-
-        # Populate the data dict and create df. Output the df as a csv file
-        dh = DataHandler(opta_ids, photo_ids, squad_numbers, first_names, last_names)
-        df = dh.create_df()
-        sorted_df = df.sort_values(
-            by="Surname",
-            key=lambda col: col.map(lambda x: collator.sort_key(x or ""))
-        )
-        # Google Sheets integration
-        worksheet = open_sheet(team_name)
         worksheet.batch_clear(['A6:Z59'])
-        worksheet.update(range_name='A6', values=sorted_df.values.tolist())
+        worksheet.update(range_name=range, values=dataframe.values.tolist())
 
         return True, f"{team_name} squad updated successfully."
 
     except Exception as e:
         return False, f"Failed to update {team_name}: {e}"
+
+def smart_capitalize(text):
+    return ' '.join(word[0].upper() + word[1:] if word else '' for word in text.split())
+
+def get_pl_squad(team_name):
+    for key in data:
+        data[key].clear()
+
+    url = squad_urls[team_name]
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, features="html.parser")
+
+    # Parse the HTML to grab all the player cards from the squad page
+    all_players = soup.find_all('li', class_='stats-card')
+
+    # Initialise lists
+    photo_ids = []
+    squad_numbers = []
+    first_names= []
+    last_names = []
+
+    for player in all_players:
+        # Get Opta IDs from the HTML
+        photo_id = player.find(name='img', class_='statCardImg statCardPlayer')
+        if photo_id:
+            photo_ids.append(photo_id['data-player'])
+        else:
+            photo_ids.append(None)
+
+        # Get squad numbers from the HTML
+        squad_number = player.find(name="div", class_='stats-card__squad-number u-hide-mob-l')
+        if squad_number:
+            squad_numbers.append(squad_number.get_text())
+        else:
+            squad_numbers.append(None)
+
+        # Get the first names from the HTML
+        first_name = player.find(name="div", class_='stats-card__player-first')
+        if first_name:
+            first_names.append(first_name.get_text().strip())
+        else:
+            first_names.append(None)
+
+        # Get the last names from the HTML
+        last_name = player.find(name="div", class_='stats-card__player-last')
+        if last_name:
+            last_names.append(last_name.get_text().strip())
+        else:
+            last_names.append(None)
+
+    # Create a list of Opta IDs by stripping the "p" out of the Photo IDs
+    opta_ids = [n.strip("p") for n in photo_ids]
+
+    # Ensure all last names start with a capital letter
+    last_names = [smart_capitalize(n) for n in last_names]
+
+    # Populate the data dict and create df. Output the df as a csv file
+    dh = DataHandler(opta_ids, photo_ids, squad_numbers, first_names, last_names)
+    df = dh.create_df()
+    sorted_df = df.sort_values(
+        by="Surname",
+        key=lambda col: col.map(lambda x: collator.sort_key(x or ""))
+    )
+    return sorted_df
 
 def read_sheet(team_name):
     pass
