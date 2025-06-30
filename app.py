54ee89c9f8e3edbad78f38d7fa8a0ddfde7282e1
Harry Wilder <harrywilder95@hotmail.co.uk>
2025-06-30 16:44:52 +0100

    Turn off debug mode.

--- app.py
+++ app.py
@@ -29,8 +29,5 @@ def index():
 
     return render_template('index.html', squad_urls=squad_urls)
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
